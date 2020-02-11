397f8468036601783e96ada1bfd855efc717fd5e
Jason Weirather <jason-weirather@users.noreply.github.com>
2020-02-11 10:13:44 -0500

    add report draft

--- pythologist_validator/schemas/__init__.py
+++ pythologist_validator/schemas/__init__.py
@@ -1,7 +1,6 @@
 import json, os
 
 # Get the directory where our schemas are located
-#print(os.path.realpath(__file__))
 __schema_dir = os.path.split(os.path.realpath(__file__))[0]
-#rint(__schema_dir)
 panel = json.loads(open(os.path.join(__schema_dir,'panel.json'),'r').read())
+report = json.loads(open(os.path.join(__schema_dir,'report.json'),'r').read())
