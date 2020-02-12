4c2ab1e718c9c43428ac2371a43dfdc2b628ed82
Jason Weirather <jason-weirather@users.noreply.github.com>
2020-02-12 09:05:47 -0500

    moving around functions for reading schema

--- pythologist_validator/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-import json, os
-
-# Get the directory where our schemas are located
-__schema_dir = os.path.split(os.path.realpath(__file__))[0]
-panel = json.loads(open(os.path.join(__schema_dir,'panel.json'),'r').read())
-report = json.loads(open(os.path.join(__schema_dir,'report.json'),'r').read())
