c6c65bcdf8deee87a8b21e4af8d603db8b0e2e41
Jason Weirather <jason-weirather@users.noreply.github.com>
2020-02-14 17:12:52 -0500

    updating drafts

--- pythologist_validator/__init__.py
+++ pythologist_validator/__init__.py
@@ -11,3 +11,9 @@ def do_inputs():
 def entry_point():
     args = do_inputs()
 
+import json, os
+
+# Get the directory where our schemas are located
+__schema_dir = os.path.split(os.path.realpath(__file__))[0]
+panel = json.loads(open(os.path.join(__schema_dir,'panel.json'),'r').read())
+report = json.loads(open(os.path.join(__schema_dir,'report.json'),'r').read())
