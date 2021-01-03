4cf57a365c73f22cbbf96639bec87874d64720ca
Jason Weirather <jason-weirather@users.noreply.github.com>
2021-01-03 14:43:37 -0500

    add in logic for combining regions

--- pythologist_schemas/report.py
+++ pythologist_schemas/report.py
@@ -12,6 +12,16 @@ def convert_report_definition_to_report(report_definition_json):
     report_definition_schema_validator.validate(report_definition_json)
 
     output = json.loads(json.dumps(report_definition_json)) # get deep copy of the definition to modify.. parameters will come directly from that
+    # Do the regions
+    for i,measure in enumerate(output['population_densities']):
+        # always should have at least one mutually exclusive phenotype so no worries about Nonetype
+        output['population_densities'][i]['mutually_exclusive_phenotypes'] = [x.strip() for x in measure['mutually_exclusive_phenotypes'].split(',')]
+        # binary could be empty if it is set it to no members
+        if measure['binary_phenotypes'] is None:
+            output['population_densities'][i]['binary_phenotypes'] = []
+        else:
+            output['population_densities'][i]['binary_phenotypes'] = \
+                [{'target_name':x.strip()[:-1].strip(),'filter_direction':x.strip()[-1]} for x in measure['binary_phenotypes'].split(',')]
     # Start with the mutually exclusive phenotypes
     for i,measure in enumerate(output['population_densities']):
         # always should have at least one mutually exclusive phenotype so no worries about Nonetype
